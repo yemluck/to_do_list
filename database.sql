09eaf794bc97a0c6f0d352e4659347e07ac874c8
Chris Agbolade <yemluck@gmail.com>
2022-01-09 20:38:12 -0600

    database.sql modified

--- database.sql
+++ database.sql
@@ -2,12 +2,23 @@ CREATE TABLE "todo" (
 	"id" SERIAL PRIMARY KEY,
 	"date" DATE, 
 	"task" VARCHAR (250) NOT NULL,
-	"priority" VARCHAR (250) NOT NULL);
+	"priority" VARCHAR (250) NOT NULL,
+	"completion_status" BOOLEAN DEFAULT FALSE);
 	
 	
 INSERT INTO "todo" 
 	("task", "date", "priority")
 VALUES
 	('Wake up','9-1-2022', 'High Priority'),
-	('Vacuum floor','9-1-2022', 'Low Priority'),
+	('Vacuum floor','9-1-2022', 'Low Priority' ),
 	('Eat breakfast','9-1-2022', 'Mid Priority');
+	
+	
+SELECT * FROM todo
+
+DROP TABLE todo
+
+
+	
+
+	
